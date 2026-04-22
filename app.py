0519dc46228da9f96500f1a8bd056eefb4070d38
kadarla_shanmukh_advance_gen_ai_internship <notshannu@gmail.com>
2026-04-22 14:26:08 +0530

    app.py

--- app.py
+++ app.py
@@ -5,18 +5,22 @@ from langchain_groq import ChatGroq
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, PyPDFLoader
 
-# API Key
-os.environ["GROQ_API_KEY"] = "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"
+# API key from Streamlit secrets
+groq_api_key = os.getenv("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")
 
 st.set_page_config(page_title="AI Support Bot")
 st.title("💬 AI Customer Support Assistant")
 
-# Upload file
-uploaded_file = st.file_uploader("Upload a file")
+# Upload multiple files
+uploaded_files = st.file_uploader(
+    "Upload TXT or PDF files",
+    type=["txt", "pdf"],
+    accept_multiple_files=True
+)
 
-# Initialize embeddings + model
+# Initialize
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
@@ -26,23 +30,36 @@ llm = ChatGroq(
     temperature=0
 )
 
-# Process uploaded file
-if uploaded_file:
-    with open("temp.txt", "wb") as f:
-        f.write(uploaded_file.read())
+if uploaded_files:
+    all_docs = []
 
-    loader = TextLoader("temp.txt", encoding="utf-8")
-    documents = loader.load()
+    # Save and load files
+    for file in uploaded_files:
+        file_path = f"temp_{file.name}"
 
+        with open(file_path, "wb") as f:
+            f.write(file.read())
+
+        if file.name.endswith(".txt"):
+            loader = TextLoader(file_path, encoding="utf-8")
+        else:
+            loader = PyPDFLoader(file_path)
+
+        docs = loader.load()
+        all_docs.extend(docs)
+
+    # Split
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=800,
         chunk_overlap=100
     )
-    chunks = splitter.split_documents(documents)
+    chunks = splitter.split_documents(all_docs)
 
+    # Vector DB
     db = FAISS.from_documents(chunks, embeddings)
     retriever = db.as_retriever(search_kwargs={"k": 3})
 
+    # RAG function
     def rag_chat(query):
         docs = retriever.invoke(query)
         context = "\n".join([doc.page_content for doc in docs])
@@ -77,4 +94,4 @@ if uploaded_file:
         st.session_state.messages.append({"role": "assistant", "content": answer})
 
 else:
-    st.info("📄 Please upload a file to start chatting")
+    st.info("📄 Upload one or more files to start chatting")
